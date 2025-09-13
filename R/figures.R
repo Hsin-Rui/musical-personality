434a5b4abd3e5d6f2bf35282fe0c65a317bf5082
Hsin-Rui <37245651+Hsin-Rui@users.noreply.github.com>
2025-09-13 22:57:31 +0200

    create_figure_7.1 function (loadings plot)

--- R/figures.R
+++ R/figures.R
@@ -1,3 +1,30 @@
 #' Create figure 7.1
 #'
+#' @param save_file if TRUE, a pdf file generated under the working directory. File name: Abbildung7.1.pdf
 #'
+#' @return base plot of factor loadings
+#' @importFrom Gifi princals
+#'
+
+create_figure_7.1 <- function(save_file = FALSE) {
+
+  df <- readRDS("inst/df_t1.rds")
+
+  df <- df [rowSums(is.na(df))==0, ] # complete case N = 2670
+  res <- Gifi::princals(df)
+
+  if (isTRUE(save_file)) {
+
+    width_in_inches <- 2160/ 300
+    height_in_inches <- 2160 / 300
+
+    pdf("inst/output/abbildung7.2.pdf", width = width_in_inches, height = height_in_inches)
+
+    plot(res, main = "Ladungen", xlab="Komponente 1", ylab="Komponente 2")
+    dev.off()
+  }
+
+  plot(res, main = "Ladungen", xlab="Komponente 1", ylab="Komponente 2")
+
+}
+
