537e9a45618d7c2862c10e3694c4de91bccc00b2
Hsin-Rui <37245651+Hsin-Rui@users.noreply.github.com>
2025-09-14 15:32:42 +0200

    run confirmatory models

--- R/models.R
+++ R/models.R
@@ -29,7 +29,7 @@ run_exploratory_mirt_27items <- function(save_model = FALSE){
 
 #' Run model with 23 items
 #'
-#' @param save_model if TRUE, then the results are saved under inst/models. File name: models_27items.rds
+#' @param save_model if TRUE, then the results are saved under inst/models. File name: models_23items.rds
 #'
 #' @importFrom mirt mirt
 #' @return a list of model objects returned by mirt::mirt()
@@ -82,7 +82,7 @@ run_exploratory_mirt_23items <- function(save_model = FALSE) {
 
 #' Run model with 22 items (IM06 deleted from the model)
 #'
-#' @param save_model if TRUE, then the results are saved under inst/models. File name: models_27items.rds
+#' @param save_model if TRUE, then the results are saved under inst/models. File name: models_22items.rds
 #'
 #' @importFrom mirt mirt
 #' @return a list of model objects returned by mirt::mirt()
@@ -130,4 +130,62 @@ run_exploratory_mirt_22items <- function(save_model = FALSE) {
 
   return(result)
 }
+
+#' Run model confirmatory MIRT with 22 items
+#'
+#' @param save_model if TRUE, then the results are saved under inst/models. File name: models_confirmatory.rds
+#'
+#' @importFrom mirt mirt
+#' @importFrom mirt bfactor
+#' @return a list of model objects returned by mirt::mirt() or mirt::bfactor()
+#'
+
+run_confirmatory_mirt_22items <- function(save_model = FALSE) {
+
+  df <- readRDS("inst/df_confirm.rds")
+
+  itemtype <- c("2PL", #1
+                "2PL", #2
+                "graded", #3
+                "graded", #4
+                "2PL", #5
+                "2PL", #6
+                "2PL", #7
+                "2PL", #8
+                "2PL", #9
+                "2PL", #10
+                "2PL", #11
+                "2PL", #12
+                "graded", #13
+                "graded", #14
+                "graded", #15
+                "graded", #16
+                "graded", #17
+                "graded", #18
+                "graded", #19
+                "graded", #20
+                "graded", #21
+                "graded") #22
+
+  set.seed(123)
+  fit1 <- mirt::mirt(df, 1, itemtype = itemtype, SE=T)
+
+  model_2F <- 'F1 = 1-12
+              F2 = 13-22
+              COV=F1*F2'
+  set.seed(123)
+  fit2 <- mirt::mirt(df, model=model_2F, itemtype = itemtype, SE=T)
+
+  model_BI <- c(rep(1,12),rep(2,10))
+  fit3 <- mirt::bfactor(df, model=model_BI, itemtype = itemtype)
+
+  result <- list(model_1 = fit1, model_2 = fit2, model_3 = fit3)
+
+  if (isTRUE(save_model)) {
+    saveRDS(result, "inst/models/models_confirmatory.rds")
+  }
+
+  return(result)
+}
+
 
